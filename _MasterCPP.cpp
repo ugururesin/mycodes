a6af08ba8df1a253ba95d4713e220818ef068bc6
ugururesin <uresinugur35@gmail.com>
2020-07-15 00:28:10 +0300

    Add MasterCpp

---
+++ _MasterCPP.cpp
@@ -0,0 +1,163 @@
+
+// WELCOME TO C++-PROGRAMMING! - 2019
+// By Ugur Uresin
+
+// CONTENT
+// 01 DATA TYPES & FORMATS
+// 02 ARITHMETIC OPERATIONS
+// 03 ARITHMETIC STATEMENTS
+// 04 APPOINTMENT, INCREASE, DECREASE
+// 05 TYPE TRANSFORMATIONS
+// 06 INPUT/OUTPUT FUNCTIONS
+// 07 EXAMPLE PROGRAM: AVERAGE CALCULATOR (SCANF)
+// 08 EXAMPLE PROGRAM: PARABOLA ROOT CALCULATOR (SCANF)
+// 09 LOGICAL STATEMENTS
+
+// 10 CONDITIONALS (IF-ELSE)
+// 11 CONDITIONALS (ELSE-IF)
+// 12 SWITCH-CASE
+// 13 LOOPS-1 (WHILE LOOP)
+// 14 LOOPS-2 (DO-WHILE LOOP)
+// 15 LOOPS-3 (FOR LOOP)
+// 16 EXAMPLE PROGRAM: FIBONACCI SERIE (FOR LOOP)
+// 17 EXAMPLE PROGRAM: SUM OF GIVEN NUMBERS (BREAK)
+// 18 EXAMPLE PROGRAM: SUM OF EVEN NUMBERS (CONTINUE)
+
+// 19 ARRAYS-1 (Integer Arrays)
+// 20 ARRAYS-2 (Float Arrays)
+// 21 ARRAYS-3 (String Arrays)
+// 22 ARRAYS-4 (Multi-Dimension Arrays)
+// 23 EXAMPLE PROGRAM: BUILDING AN 3x5 MATRIX (ARRAYS)
+
+// 24 FUNCTIONS-1
+// 25 FUNCTIONS-2 (Summation & Factorial Functions)
+// 26 FUNCTIONS-3 (Prime Number Detection)
+// 27 FUNCTIONS-4 (Arrays as parameter)
+// 28 FUNCTIONS-5 (Strings as parameter)
+
+// 29 POINTERS-1 (INTRODUCTION)
+// 30 POINTERS-2
+// 31 POINTERS-3 (Call By Value)
+// 32 POINTERS-4 (Call By Reference)
+
+// 33 ARRAYS & POINTERS
+// 34 EXAMPLE PROGRAM: MAX FINDER BY ARRAY POINTER
+// 35 STRINGS & POINTERS
+// 36 FUNCTIONS WITH POINTER RETURN
+// 37 POINTER ARRAYS (ARRAYS THAT STORE POINTERS)
+// 38 EXAMPLE PROGRAM: BUBBLE SORTING
+
+// 39 STRUCTURES-1
+// 40 STRUCTURES-2
+// 41 NESTED STRUCTURES
+// 42 ARRAY STRUCTURES
+// 43 FUNCTIONS & STRUCTURES
+// 44 POINTERS & STRUCTURES
+// 45 CALL BY REFERENCE IN STRUCTURES
+
+// 46 FILES-1: fopen function
+// 47 FILES-2: fputc function
+// 48 FILES-3: fputs function
+
+
+// -------------------------------------------------- //
+// 01 HELLO WORLD!
+#include <iostream>
+
+int main() 
+{
+  std::cout << "Hello world, I am ready for C++";
+  return 0;
+}
+
+
+
+// 02 NAMESPACE
+/*Use the namespace keyword to simplify typing*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+ cout << "Hey, writing std:: is pain, ";
+ cout << "change the program so I don't have to write it.";
+ return 0;
+}
+
+
+// 03 PRINT VARIABLES
+/*GOAL: Practice writing to the console and learn 
+**the variables types available in C++
+**Print the sizes of each variable to the console.
+**Print them in the following order:
+**int, short, long, char, float, double, bool
+*/
+#include <iostream>
+
+int main()
+{
+ using namespace std;
+ cout<<"int size = "<<sizeof(int)<<"\n";
+ cout<<"short size = "<<sizeof(short)<<"\n";
+ cout<<"long size = "<<sizeof(long)<<"\n";
+ cout<<"char size = "<<sizeof(char)<<"\n";
+ cout<<"float size = "<<sizeof(float)<<"\n";
+ cout<<"double size = "<<sizeof(double)<<"\n";
+ cout<<"bool size = "<<sizeof(bool)<<"\n";
+ return 0;
+}
+
+// 04 FORMATTING THE OUTPUT
+/*Formatting Output 
+**Goal: practice using cout to format output to console
+**Print the variables in three columns:
+**Ints, Floats, Doubles
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a = 45;
+    float b = 45.323;
+    double c = 45.5468;
+    int aa = a + 9;
+    float bb = b + 9;
+    double cc = c + 9;
+    int aaa = aa + 9;
+    float bbb = bb + 9;
+    double ccc = cc + 9;
+
+    cout << "Ints" << setw(13) << "Floats" << setw(15) << "Doubles" << "\n";
+    cout << a  << setw(15) << b << setw(15) << c << "\n";
+    cout << aa << setw(15) << bb << setw(15) << cc <<"\n";
+    cout << aaa << setw(15) << bbb << setw(15) << ccc << "\n";
+        
+    return 0;
+}
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+// 03 PRINT VARIABLES
+
+
+
