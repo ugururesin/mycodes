12d20248a0a4f1c3464c4ba3e0fd0e80f2b39a63
ugururesin <uresinugur35@gmail.com>
2020-07-15 14:55:53 +0300

    Add Flows

--- _MasterCPP.cpp
+++ _MasterCPP.cpp
@@ -139,25 +139,172 @@ int main()
 }
 
 
-// 03 PRINT VARIABLES
+// 05 FILE IO
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    string line;
+    //create an output stream to write to the file
+    //append the new lines to the end of the file
+    ofstream myfileI ("input.txt", ios::app);
+    if (myfileI.is_open())
+    {
+        myfileI << "\nI am adding a line.\n";
+        myfileI << "I am adding another line.\n";
+        myfileI.close();
+    }
+    else cout << "Unable to open file for writing";
+
+    //create an input stream to read the file
+    ifstream myfileO ("input.txt");
+    //During the creation of ifstream, the file is opened. 
+    //So we do not have explicitly open the file. 
+    if (myfileO.is_open())
+    {
+        while ( getline (myfileO,line) )
+        {
+            cout << line << '\n';
+        }
+        myfileO.close();
+    }
+
+    else cout << "Unable to open file for reading";
+
+    return 0;
+}
 
 
-// 03 PRINT VARIABLES
+// 06 POINTERS
+/*  & -> for adress
+    * -> value for that adress
+*/    
+int myVal = 5;
+std::cout << "&a";   //where is a?
+
+int * pointerToA = &a;
+std::cout << * pointerToA;
 
 
-// 03 PRINT VARIABLES
+// 07 ARRAYS
+// variableType arrayName[] = {vars to be stored};
+// variableType arrayName [arraysize]
+
+// Multi-Dimensional Array
+// typeOfVariable arrayName[size of dim.1][size of dim.2]...;
+
+// Example
+int array2Dimensions[2][3];             //declaration
+int array2Dim[2][3] = {0,1,2,3,4,5};    //definition
+for (int i=0; i<2;  i++)
+    for (int j=0; j<3; j++)
+        std::cout <<"array2Dim["<<i<<"]["<<j<<"]= " <<array2Dim[i][j] <<"\n";
 
 
-// 03 PRINT VARIABLES
+// 08 FUNCTIONS
+/* Format
+returnedVariableType functionName (param1, ... ,paramN)
+{
+    statement(s);
+}
+*/
+//Example
+void printMessage()
+{
+    std::cout << "Hello world!";
+}
 
 
-// 03 PRINT VARIABLES
+// 09 IF-STATEMENTS
+#include<iostream>
+
+int main()
+{
+    std::cout<<"This program checks the value of a.\n";
+    int a = 10;
+
+    if(a == 0)
+    {
+        std::cout << "a is equal to 0!\n";
+    }
+    else
+    {
+        std::cout << "a is NOT equal to 0!\n";
+    }
+    return 0;
+}
 
 
-// 03 PRINT VARIABLES
+// 10 SWITCH STATEMENTS
+/*Goal: demonstrate use cases for the switch statement.*/
+
+#include <iostream>
+
+int main()
+{
+    int menuItem = 1;
+    
+    std::cout<<"What is your favorite winter sport?: \n";
+    std::cout<<"1.Skiing\n2: Sledding\n3: Sitting by the fire";
+    std::cout<<"\n4.Drinking hot chocolate\n";
+    std::cout<<"\n\n";
+    
+    switch(menuItem)
+    {
+        case(1): std::cout<<"Skiing?! Sounds dangerous!\n";
+                 break;
+        case(2): std::cout<<"Sledding?! Sounds like work!\n";
+                 break; 
+        case(3): std::cout<<"Sitting by the fire?! Sounds warm!\n";
+                 break;
+        case(4): std::cout<<"Hot chocolate?! Yum!\n";
+                 break;
+        default: std::cout<<"Enter a valid menu item";
+    }
+    
+    char begin;
+    std::cout<<"\n\nWhere do you want to begin?\n";
+    std::cout<<"B. At the beginning?\nM. At the middle?";
+    std::cout<<"\nE. At the end?\n\n";
+    begin = 'M';  
+    
+    switch(begin)
+    {
+        case('B'): std::cout<<"Once upon a time there was a wolf.\n";
+        case('M'): std::cout<<"The wolf hurt his leg.\n";
+        case('E'): std::cout<<"The wolf lived happily everafter\n";
+    }
+    return 0;
+}
 
 
-// 03 PRINT VARIABLES
+// 11 FOR LOOPS
+// TYPE.1 -> for ( declaration : range ) statement;
+// TYPE.2 -> for (initialization; condition; increase) statement;
+
+//Example
+#include <iostream>
+int main()
+{
+    float input;
+    float sum = 0;
+
+    for(int i=0; i<5; i++)
+    {
+        std::cout<<"What is the next number?\n";
+        std::cin>>input;
+        sum = sum + input;
+    }//end of for loop
+
+    std::cout<<"Sum = "<<sum<<"\n";
+    std::cout<<"Average = "<<sum/5<<"\n";
+    return 0;
+}
+
+// 11 FOR LOOPS
+
 
 
 
