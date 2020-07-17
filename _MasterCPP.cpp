36b399959c55fe5d629f80e179e8230ca31970e1
ugururesin <uresinugur35@gmail.com>
2020-07-17 16:26:53 +0300

    Update _MasterCPP.cpp

--- _MasterCPP.cpp
+++ _MasterCPP.cpp
@@ -303,7 +303,46 @@ int main()
     return 0;
 }
 
-// 11 FOR LOOPS
+// 12 FOR LOOPS
+// REGULAR WHILE LOOP
+while(condition)
+{
+     statements;
+}
+
+// DO ... WHILE LOOP
+do
+{
+     statements;
+}while(condition );
+
+//Example
+#include <iostream>
+int main()
+{
+    int count = 0;
+    //This do..while loop will execute until count =5
+    do
+    {
+        std::cout<<"Count = "<<count<<"\n";
+        count++;
+    }while(count < 5);
+
+
+    int otherCount = 6; 
+    //This do..while loop will execute once. Even though
+    //otherCount > 5
+    do
+    {
+        std::cout<<"othercount = "<<otherCount<<"\n";
+        otherCount++;
+    }while(otherCount < 5);
+    
+    return 0;
+}
+
+// 13 EXITING LOOPS
+
 
 
 
