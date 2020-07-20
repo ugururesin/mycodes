1f09b90c485fc352fbe8a6321557b85e0f9cdfbf
ugururesin <uresinugur35@gmail.com>
2020-07-20 16:40:46 +0300

    Add private and encapsulation

--- _MasterCPP.cpp
+++ _MasterCPP.cpp
@@ -18,7 +18,7 @@ For training purposes! */
 // 12 FOR LOOPS
 // 13 EXITING LOOPS
 // 14 CLASSES
-
+// 15 PRIVATE AREAS IN CLASS & ENCAPSULATION
 
 // -------------------------------------------------- //
 // 01 HELLO WORLD!
@@ -359,6 +359,48 @@ class Student
         void print();
 };
 
+// 15 PRIVATE AREAS IN CLASS & ENCAPSULATION
+//Example
+/* In the example below,
+the name can't be reached directly!
+Thus, we keep it as it is and also protect it.
+However, it's still reachable inside class.
+For example the method getName() can reach the "name".
+*/
+class Employee{
+private:
+    string name;            
 
+public:
+    void getName(){
+        cout << name << endl;
+    }
+};
 
+/* To make attributes in the private area reachable,
+define set and get methods as shown below.
+The process of making attributes private and defining set and get methods
+is called 'ENCAPSULATION'.
+See the example below*/
+class Employee{
+private:
+    string name; //name and age attributes are private!
+    int age;    //so, it's not reachable directly!
+public:
+    /* Here, get and set methods are defined so that
+    user can read and change the values of the attributes indirectly.
+    */
+    void getName(){
+        return name;
+    }
+    void setName(string newname){
+        name = newname;
+    }
 
+    void getAge(){
+        return age;
+    }
+    void setAge(int newage){
+        age = newage;
+    }
+};
