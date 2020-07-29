b118291c4b16998e1ec80092d4bbb8c3a75cbf07
ugururesin <uresinugur35@gmail.com>
2020-07-29 19:58:01 +0300

    Add templates

--- _MasterCPP.cpp
+++ _MasterCPP.cpp
@@ -24,6 +24,8 @@ To execute the program: ./executableName
 // 13 EXITING LOOPS
 // 14 CLASSES
 // 15 PRIVATE AREAS IN CLASS & ENCAPSULATION
+// 16 TEMPLATES (Same VarType)
+// 17 TEMPLATES (Multiple VarType)
 
 // -------------------------------------------------- //
 // 01 HELLO WORLD!
@@ -409,3 +411,135 @@ public:
         age = newage;
     }
 };
+
+// 16 TEMPLATES (Same VarType)
+/*
+The function declaration:
+template <typename T>  //tell the compiler we are using a template
+T  functionName (T parameter1,T parameter2, ...); 
+
+The function definition:
+template <typename T>
+T functionName (T  parameter1,T  parameter2,...)
+{
+    function statements;
+}
+
+T represents the variable type.
+Since we want it to be for any type, we use T
+*/
+
+//Example-1
+int findSmaller(int input1, int input2)
+{
+    if(input1<input2)
+        return input1;
+    return input2;
+}
+//What if we'd like to use floats instead of ints?
+//If this is the case, we need to generalize the function.
+#include<iostream>
+
+using namespace std;
+
+//Our generic function
+template <typename T>  //tell the compiler we are using a template
+T findSmaller(T input1,T  input2); 
+
+int main()
+{
+    int a = 54; 
+    int b = 89;
+    float f1 = 7.8;
+    float f2 = 9.1;
+    char c1 = 'f';
+    char c2 = 'h';
+    string s1 = "Hello";
+    string s2 = "Bots are fun";
+    
+    //One function can be used for different variable types!
+    cout<<"\nIntegers compared: "<<findSmaller(a,b);
+    cout<<"\nFloats compared: "<<findSmaller(f1,f2);
+    cout<<"\nChars compared: "<<findSmaller(c1,c2);
+    cout<<"\nStrings compared: "<<findSmaller(s1,s2);   
+    return 0;
+}
+
+template <typename T>
+T findSmaller(T  input1,T  input2)
+{
+    if(input1 < input2)
+        return input1;
+    else
+        return input2;
+}
+
+//Example-2
+#include<iostream>
+
+//Our generic function
+template <typename T>  //tell the compiler we are using a template
+//use addresses of the variables in the delcaration of the function
+
+template <typename T>
+T sumTwo(T  a,T  b)
+{
+    //Note  that the sum is a variable type T. This means whatever 
+    //variable type is passed in
+   //will become the variable type for sum.
+    T  sum;
+    sum = a + b;
+    return sum;
+}
+
+// 17 TEMPLATES (Multiple VarType)
+//Format
+template <typename T, typename U, typename V>
+T functionName (U parameter1, V parameter2, ...)
+{
+    function statements;
+}
+
+//Example
+T getBigger(T input1, U input2)
+{
+    if(input1 > input2)
+        return input1;
+    return input2;
+}
+
+//Example-2
+/*Goal: learn to use templates with multiple variable types. 
+*/
+
+#include<iostream>
+using namespace std;
+
+template <typename T, typename U>
+T getBigger(T input1, U input2);
+
+
+int main()
+{
+    int a = 5;
+    float b = 6.334;
+    int bigger;
+    cout<<"Between "<<a<<" and "<<b<<" "<<getBigger(a,b)<<" is bigger.\n";
+
+    cout<<"Between "<<a<<" and "<<b<<" "<<getBigger(b,a)<<" is bigger.\n";    
+    return 0;
+}
+
+template <typename T, typename U>
+T getBigger(T input1, U input2)
+{
+    if(input1 > input2)
+        return input1;
+    return input2;
+}
+
+/*The output of the above code
+Between 5 and 6.334 6 is bigger.
+Between 5 and 6.334 6.334 is bigger.
+*/
+
