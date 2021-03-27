080895e56a591955bb8b7df798c8c2b08d7993d3
ugur <55594186+ugururesin@users.noreply.github.com>
2021-03-27 18:03:58 +0300

    up vars

--- _MasterCS.cs
+++ _MasterCS.cs
@@ -15,13 +15,18 @@ class Program
 
 // VARIABLES
 string Mystring;
-Mystring = "Hello World!";
-
-int Myinteger;
-Myinteger = 10;
+Mystring = "Hello World!"; //or string Mystring="Hello World!"
 
 char Mycharacter;
 Mycharacter = 'A';
+
+int Myinteger1, Myinteger2;
+Myinteger1 = 2147483647;	//max positive integer
+Myinteger2 = -2147483647;	//max negative integer
+
+long Mylong1, Mylong2;
+Mylong1 = 9223372036854775806;		//max positive long
+Mylong2 = -9223372036854775806;		//min positive long
 
 bool result;
 result = true;
@@ -30,5 +35,10 @@ result = true;
 // PRINTING A VALUE
 Console.WriteLine(Mystring);
 Console.WriteLine(Mycharacter);
-Console.WriteLine(Myinteger);
+Console.WriteLine(Myinteger1);
+Console.WriteLine(Myinteger2);
+Console.WriteLine(Mylong1);
+Console.WriteLine(Mylong2);
 Console.WriteLine(result);
+
+
