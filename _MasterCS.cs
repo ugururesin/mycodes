3f74af11dafaa4fe313be9d3ea6432c93f9ffd18
ugur <55594186+ugururesin@users.noreply.github.com>
2021-03-27 17:36:49 +0300

    add variables

--- _MasterCS.cs
+++ _MasterCS.cs
@@ -1,1 +1,34 @@
-//C# TEMPLATE
+// C# TEMPLATE
+//----------------------------------------//
+
+// MAIN
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+// VARIABLES
+string Mystring;
+Mystring = "Hello World!";
+
+int Myinteger;
+Myinteger = 10;
+
+char Mycharacter;
+Mycharacter = 'A';
+
+bool result;
+result = true;
+
+
+// PRINTING A VALUE
+Console.WriteLine(Mystring);
+Console.WriteLine(Mycharacter);
+Console.WriteLine(Myinteger);
+Console.WriteLine(result);
